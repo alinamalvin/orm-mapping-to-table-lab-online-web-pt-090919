c30d919d2e7cbe6489771db1212fcd512182d9ef
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-07 22:01:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,21 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+    attr_accessor :name, :grade
+    
+    def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @album = album
+  end
   
-end
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+end 
+
